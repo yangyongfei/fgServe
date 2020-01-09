13b18a7085be2ecf261e1276813a0dafe1b9093d
yangyongfei <94123792@qq.com>
2020-01-09 19:00:00 +0800

    mysql增删改查

--- app/service/home.js
+++ app/service/home.js
@@ -5,6 +5,22 @@ class HomeService extends Service {
 		const user = this.app.mysql.get('user', { id: 1 });
 		return user;
 	 }
+	 async list(){
+	 		const user = this.app.mysql.select('user');
+	 		return user;
+	 }
+	 async add(user){
+	 		const status = await this.app.mysql.insert('user',user); 
+	 		return status;
+	 }
+	 async edit(){
+	 		const user = this.app.mysql.update('user', { id: 1 });
+	 		return user;
+	 }
+	 async delate(){
+	 		const user = this.app.mysql.delete('user', { id: 1 });
+	 		return user;
+	 }
 }
 
 module.exports = HomeService;
